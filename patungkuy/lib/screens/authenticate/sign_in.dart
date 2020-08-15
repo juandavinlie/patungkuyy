142389c9c5f23e65005c2341774875d69a0a738a
Vieri Harney <vieriharney@gmail.com>
2020-08-15 19:55:09 +0700

    fix login screen blocked by keyboard

--- patungkuy/lib/screens/authenticate/sign_in.dart
+++ patungkuy/lib/screens/authenticate/sign_in.dart
@@ -43,62 +43,66 @@ class _SignInState extends State<SignIn> {
                 padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
                 child: Form(
                     key: _formKey,
-                    child: Column(children: <Widget>[
-                      //SizedBox(height: 20.0),
-                      Image(image: AssetImage('assets/patungkuy.png')),
-                      TextFormField(
-                          decoration:
-                              textInputDecoration.copyWith(hintText: 'Email'),
-                          validator: (val) =>
-                              val.isEmpty ? "Please enter an email!" : null,
-                          onChanged: (val) {
-                            setState(() {
-                              email = val;
-                            });
-                          }),
-                      SizedBox(height: 20.0),
-                      TextFormField(
-                          decoration: textInputDecoration.copyWith(
-                              hintText: 'Password'),
-                          validator: (val) => val.length < 6
-                              ? "Please enter a password of 6 or more characters!"
-                              : null,
-                          obscureText: true,
-                          onChanged: (val) {
-                            setState(() {
-                              password = val;
-                            });
-                          }),
-                      SizedBox(height: 20.0),
-                      RaisedButton(
-                          color: Colors.pink[400],
-                          child: Text(
-                            'Sign In',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          onPressed: () async {
-                            // return true only when null is returned by the validators
-                            if (_formKey.currentState.validate()) {
+                    child: SingleChildScrollView(
+                      child: Column(children: <Widget>[
+                        //SizedBox(height: 20.0),
+                        Image(image: AssetImage('assets/patungkuy.png')),
+                        TextFormField(
+                            decoration:
+                                textInputDecoration.copyWith(hintText: 'Email'),
+                            validator: (val) =>
+                                val.isEmpty ? "Please enter an email!" : null,
+                            onChanged: (val) {
                               setState(() {
-                                loading = true;
-                                userEmail = email;
+                                email = val;
                               });
-                              dynamic result = await _auth
-                                  .signInWithEmailAndPassword(email, password);
-
-                              if (result == null) {
+                            }),
+                        SizedBox(height: 10.0),
+                        TextFormField(
+                            decoration: textInputDecoration.copyWith(
+                                hintText: 'Password'),
+                            validator: (val) => val.length < 6
+                                ? "Please enter a password of 6 or more characters!"
+                                : null,
+                            obscureText: true,
+                            onChanged: (val) {
+                              setState(() {
+                                password = val;
+                              });
+                            }),
+                        SizedBox(height: 20.0),
+                        RaisedButton(
+                            color: Colors.pink[400],
+                            child: Text(
+                              'Sign In',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onPressed: () async {
+                              // return true only when null is returned by the validators
+                              if (_formKey.currentState.validate()) {
                                 setState(() {
-                                  error =
-                                      "SignIn failed, please make sure your email and password are correct!";
-                                  loading = false;
+                                  loading = true;
+                                  userEmail = email;
                                 });
+                                dynamic result =
+                                    await _auth.signInWithEmailAndPassword(
+                                        email, password);
+
+                                if (result == null) {
+                                  setState(() {
+                                    error =
+                                        "SignIn failed, please make sure your email and password are correct!";
+                                    loading = false;
+                                  });
+                                }
                               }
-                            }
-                          }),
-                      SizedBox(height: 12.0),
-                      Text(error,
-                          style: TextStyle(color: Colors.red, fontSize: 14.0)),
-                    ]))),
+                            }),
+                        SizedBox(height: 12.0),
+                        Text(error,
+                            style:
+                                TextStyle(color: Colors.red, fontSize: 14.0)),
+                      ]),
+                    ))),
           );
   }
 }
